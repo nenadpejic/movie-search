5bb6f7f5a543f5d75c277e53aced61432e97ee16
nenadpejic <npejic89@gmail.com>
2020-10-16 00:55:22 +0200

    Added handleScroll(), start and end states and useRef() hook to Movies

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,26 +1,43 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import "./style.css";
 import Movie from "./Movie";
 
 const Movies = ({ movies }) => {
+  const myRef = useRef();
+  const [start, setStart] = useState(true);
+  const [end, setEnd] = useState(false);
+
   const handleClick = (param) => {
-    let ul = document.querySelector("#Movies ul");
-    param ? (ul.scrollLeft -= 900) : (ul.scrollLeft += 900);
+    param
+      ? (myRef.current.scrollLeft -= 900)
+      : (myRef.current.scrollLeft += 900);
+  };
+
+  const handleScroll = (e) => {
+    const elem = e.target;
+    elem.scrollLeft > 0 ? setStart(false) : setStart(true);
+    Math.ceil(elem.scrollLeft) >= elem.scrollWidth - elem.clientWidth
+      ? setEnd(true)
+      : setEnd(false);
   };
 
   return (
     <div id="Movies">
-      <ul>
+      <ul ref={myRef} onScroll={(e) => handleScroll(e)}>
         {movies.map((movie) => (
           <Movie key={movie.imdbID} movie={movie} />
         ))}
       </ul>
-      <button className="btnLeft" onClick={() => handleClick(true)}>
-        {"<"}
-      </button>
-      <button className="btnRight" onClick={() => handleClick(false)}>
-        {">"}
-      </button>
+      {!start ? (
+        <button className="btnLeft" onClick={() => handleClick(true)}>
+          {"<"}
+        </button>
+      ) : null}
+      {!end ? (
+        <button className="btnRight" onClick={() => handleClick(false)}>
+          {">"}
+        </button>
+      ) : null}
     </div>
   );
 };
