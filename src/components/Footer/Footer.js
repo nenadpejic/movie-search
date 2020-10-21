b933983b0106f1d63d41d6f13923fb0e484df45f
nenadpejic <npejic89@gmail.com>
2020-10-21 15:54:32 +0200

    Created Footer component and styled it

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>Created by Nenad Pejic</p>
+    </footer>
+  );
+};
+
+export default Footer;
