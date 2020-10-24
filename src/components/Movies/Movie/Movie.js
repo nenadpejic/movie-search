844bb154792ff6b553062562356160082bcedaf9
nenadpejic <npejic89@gmail.com>
2020-10-24 12:20:19 +0200

    Added placeholder for when poster not available

--- src/components/Movies/Movie/Movie.js
+++ src/components/Movies/Movie/Movie.js
@@ -1,10 +1,18 @@
 import React from "react";
 import "./style.css";
+import imgSrc from "../../../images/placeholder.jpg";
 
 const Movie = ({ movie }) => {
   return (
     <li>
-      <img src={movie.Poster} alt="poster" />
+      {movie.Poster === "N/A" ? (
+        <div className="pna">
+          <img className="pna-img" src={imgSrc} alt="placeholder" />
+          <div className="pna-content">Poster Not Available</div>
+        </div>
+      ) : (
+        <img src={movie.Poster} alt="poster" />
+      )}
       <p>
         {movie.Title} <span>({movie.Year})</span>
       </p>
