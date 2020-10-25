472e598fa2c7bd0b6e80ba7c6a2678bded551b6b
nenadpejic <npejic89@gmail.com>
2020-10-25 11:17:53 +0100

    Added links to IMDb

--- src/components/Movies/Movie/Movie.js
+++ src/components/Movies/Movie/Movie.js
@@ -5,17 +5,19 @@ import imgSrc from "../../../images/placeholder.jpg";
 const Movie = ({ movie }) => {
   return (
     <li>
-      {movie.Poster === "N/A" ? (
-        <div className="pna">
-          <img className="pna-img" src={imgSrc} alt="placeholder" />
-          <div className="pna-content">Poster Not Available</div>
-        </div>
-      ) : (
-        <img src={movie.Poster} alt="poster" />
-      )}
-      <p>
-        {movie.Title} <span>({movie.Year})</span>
-      </p>
+      <a href={"https://www.imdb.com/title/" + movie.imdbID} target="_blank">
+        {movie.Poster === "N/A" ? (
+          <div className="pna">
+            <img className="pna-img" src={imgSrc} alt="placeholder" />
+            <div className="pna-content">Poster Not Available</div>
+          </div>
+        ) : (
+          <img src={movie.Poster} alt="poster" />
+        )}
+        <p>
+          {movie.Title} <span>({movie.Year})</span>
+        </p>
+      </a>
     </li>
   );
 };
